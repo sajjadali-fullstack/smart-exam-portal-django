2d6543d8564b661ffd8e36bed6183b09e9ddcc9c
Sajjad Ali <yoursajju61@gmail.com>
2026-08-05 19:53:28 +0530

    feat: Customize Django admin interface

--- exam/admin.py
+++ exam/admin.py
@@ -1,10 +1,54 @@
 from django.contrib import admin
+from exam.models import Category, Question, TestResult, Feedback
 
-# Register your models here.
-from django.contrib import admin
-from .models import Category, Question, TestResult, Feedback
 
-admin.site.register(Category)
-admin.site.register(Question)
-admin.site.register(TestResult)
-admin.site.register(Feedback)
+# CategoryAdmin
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "description")
+    search_fields = ("name",)
+    ordering = ("name",)
+
+
+# QuestionAdmin
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("id", "category", "difficulty", "question_text", "correct_option",)
+
+    list_filter = ("category", "difficulty",)
+
+    search_fields = ("question_text", "category__name",)
+
+    ordering = ("category", "difficulty")
+
+
+# TestResultAdmin
+@admin.register(TestResult)
+class TestResultAdmin(admin.ModelAdmin):
+
+    list_display = ("id", "user", "category", "difficulty_level", "percentage", "date_attempted",)
+
+    list_filter = ("category", "difficulty_level", "date_attempted",)
+
+    search_fields = ("user__username", "category__name",)
+
+    ordering = ("-date_attempted",)
+
+
+# FeedbackAdmin
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    # id, name, email, rating, message, created_at
+    list_display = ("id", "name", "rating", "created_at",)
+
+    list_filter = ("rating", "created_at",)
+
+    search_fields = ("name", "message",)
+
+    ordering = ("-created_at",)
+
+
+
+
+
+    
