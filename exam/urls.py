31f33254dfd33663358f5fcb0f1ad88ac15a5081
Sajjad Ali <yoursajju61@gmail.com>
2026-08-06 12:57:54 +0530

    refactor: organize application routes

--- exam/urls.py
+++ exam/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
-
+from .views import UserLoginView
 urlpatterns = [
     path('', views.homepage, name='homepage'),
 
@@ -19,7 +19,7 @@ urlpatterns = [
 
     # Login aur Logout standard views
     path('register/', views.register_view, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='exam/login.html'), name='login'),
+    path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='homepage'), name='logout'),
     
     # Registration Route (Iska view hum abhi niche banayenge)
