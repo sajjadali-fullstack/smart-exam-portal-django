4ed457b0eb13ecee4931259e2175cb9a2947d655
Sajjad Ali <yoursajju61@gmail.com>
2026-08-06 12:55:32 +0530

    feat: enhance authentication flow and improve about page

--- exam/forms.py
+++ exam/forms.py
@@ -37,3 +37,59 @@ class FeedbackForm(forms.ModelForm):
             }),
 
         }
+
+
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class RegisterForm(UserCreationForm):
+
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields["username"].widget.attrs.update({
+            "class": "form-control",
+            "placeholder": "Choose a username"
+        })
+
+        self.fields["email"].widget.attrs.update({
+            "class": "form-control",
+            "placeholder": "Enter your email address"
+        })
+
+        self.fields["password1"].widget.attrs.update({
+            "class": "form-control",
+            "placeholder": "Create a strong password"
+        })
+
+        self.fields["password2"].widget.attrs.update({
+            "class": "form-control",
+            "placeholder": "Confirm your password"
+        })
+
+from django.contrib.auth.forms import AuthenticationForm
+
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
+class LoginForm(AuthenticationForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields["username"].widget.attrs.update({
+            "class": "form-control",
+            "placeholder": "Enter your username"
+        })
+
+        self.fields["password"].widget.attrs.update({
+            "class": "form-control",
+            "placeholder": "Enter your password"
+        })
